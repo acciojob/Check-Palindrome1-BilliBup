23d8fd150a8d9d8df918bef57ca67ca03b411e16
SHRISHTY MALVIYA <103518988+BilliBup@users.noreply.github.com>
2023-04-25 18:24:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function palindrome(str){
   var i = 0;
   var j =  str.length()-1;
   while(i<=j){
-	  if(str.charAt(i)!=str.charAt(j)){
+	  if(str.charAt(i)!==str.charAt(j)){
 		  return false;
 	  }
 	  i++;
