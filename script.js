df058dd78e578f0a6ec92949367d02fa0e2758ba
SHRISHTY MALVIYA <103518988+BilliBup@users.noreply.github.com>
2023-04-25 18:30:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // complete the given function
 
 function palindrome(str){
+   if (typeof str !== 'string') {
+    return false; // return false if input is not a string
+  }
+  
+  str = str.toLowerCase(); // convert input to lowercase
   var i = 0;
   var j = str.length - 1;
   while (i <= j) {
